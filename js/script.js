25d94d6a2450874ad132c4e39db01390c9a307fb
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-09 15:48:17 +0100

    Stampato messaggio e inserita immagine da data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("vue ok", Vue);
+/*
+Stampare a schermo un messaggio all’interno di un h1, 
+utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "Benvenuto in VUE",
+      image:
+        "https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png",
+    };
+  },
+});
+
+app.mount("#root");
